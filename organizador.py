6e6e074dab7ad9b5732b54b80bb04ebe45858799
Pqoster <48698601+Pqoh@users.noreply.github.com>
2023-07-20 00:51:12 -0300

    Delete organizador.py

--- organizador.py
+++
@@ -1,5 +0,0 @@
-with open('palavras.txt') as file:
-    lines = sorted(set(line.rstrip().lower() for line in file))
-
-with open('palavras.txt', 'w') as file:
-    file.writelines(line + '\n' for line in lines)
